14883d203b66be76a63d43d0c77e2f4a06154943
Austin <autbrown@ucsc.edu>
2019-03-13 17:13:38 -0400

    Edit metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Austin Brown`,
+    description: `Content engineered by Austin Brown`,
+    author: `@ddlinkz`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
